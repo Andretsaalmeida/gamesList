4cf82d8c306b89055d53254391630d7e9ae46410
Andrezza Batista Dantas <andretsabatista@gmail.com>
2025-08-19 20:22:24 -0300

    Domain model

---
+++ gamelist/src/main/java/com/andretsaalmeida/gamelist/entities/Belonging.java
@@ -0,0 +1,17 @@
+package com.andretsaalmeida.gamelist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_belonging")
+@Data
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+}
---
+++ gamelist/src/main/java/com/andretsaalmeida/gamelist/entities/GameList.java
@@ -0,0 +1,14 @@
+package com.andretsaalmeida.gamelist.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
